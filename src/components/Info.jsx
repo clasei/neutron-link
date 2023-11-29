cdfee3b82084fad8680572d1ac835b0fbc39870d
clara <clasei@proton.me>
2023-11-29 11:29:09 +0100

    'repeat' button changed to refresh only input

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -2,15 +2,15 @@ import { } from 'react'
 import neutronLinkImage from '../assets/images/neutron-link.png';
 
 function Info() {
-  function refreshPage() {
-    window.location.reload(false);
+  function clearInputFields() {
+    document.querySelectorAll('input').forEach(input => input.value = '');
   }
 
     return (
       <div className="info">
         <p>3. SHARE YOUR NEUTRON LINK</p>
         <img src={neutronLinkImage} alt="this is a neutron" className="neutron-image" />
-        <button onClick={refreshPage}>repeat</button>
+        <button onClick={clearInputFields}>repeat</button>
       </div>
     )
   }
