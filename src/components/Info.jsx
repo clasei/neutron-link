7d38b8347569ff36d320b7b638b676a7dd40d43a
clara <clasei@proton.me>
2023-12-06 18:40:43 +0100

    style updated

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -8,7 +8,7 @@ function Info() {
 
     return (
       <div className="info">
-        <p className="steps">3. SHARE & ENJOY</p>
+        <p className="steps">SHARE & ENJOY</p>
         <div>
           <img src={neutronLinkImage} alt="this is a neutron" className="neutron-image" />
         </div>
